5029f380f1811362a905ae1bf7abe60963c52c97
Israel Garcia <igarcia@itexico.net>
2017-02-10 09:13:01 -0600

    Comunicating child components using input

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
 
 import {EventsAppComponent} from './events-app.component'
+import {EventListComponent} from './events/events-list.component'
+import {EventThumbnailComponent} from './events/event-thumbnail.component'
 
 @NgModule({
     imports:[BrowserModule],
-    declarations:[EventsAppComponent],
+    declarations:[EventsAppComponent, EventListComponent, EventThumbnailComponent],
     bootstrap:[EventsAppComponent]
 })
 
---
+++ app/events/events-list.component.ts
@@ -0,0 +1,30 @@
+import { Component } from "@angular/core"
+
+@Component({
+    selector:"events-list",
+    template:` 
+    <div>
+        <h1>Upcoming Angular 2 events</h1>
+        <hr>
+        <event-thumbnail [event]="event1"></event-thumbnail>
+    </div>
+    `
+})
+
+export class EventListComponent{
+
+    event1={
+        id:1,
+        name:'Angular Connect!!!!',
+        date: '9/26/2010',
+        time: '10:00 am',
+        price: 500.00,
+        imageUrl:'app/assets/images/angularconnect-shield.png',
+        location:{
+            address:'1212 dt',
+            city: 'GDL',
+            country:'MX'
+        }
+    }
+
+}
