565154df1684e4dbfbf69b2500725ba544c203ad
Israel Garcia <igarcia@itexico.net>
2017-02-15 11:01:23 -0600

    First Service

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,11 +5,13 @@ import {EventsAppComponent} from './events-app.component'
 import {EventListComponent} from './events/events-list.component'
 import {EventThumbnailComponent} from './events/event-thumbnail.component'
 import {NavBarComponent} from './nav/navbar.component'
+import {EventService} from './events/shared/event.service'
 
 @NgModule({
     imports:[BrowserModule],
     declarations:[EventsAppComponent, EventListComponent, EventThumbnailComponent, NavBarComponent],
-    bootstrap:[EventsAppComponent]
+    bootstrap:[EventsAppComponent],
+    providers:[EventService]
 })
 
 export class AppModule{}
